7849cf490adac6017f0de44ebec973117ccc9af7
seb <contact@smaloron.eu>
2023-02-22 10:14:50 +0100

    phpunit providers

--- tests/EmailValidatorTest.php
+++ tests/EmailValidatorTest.php
@@ -7,11 +7,51 @@ require "vendor/autoload.php";
 
 class EmailValidatorTest extends TestCase
 {
+    private EmailValidator $validator;
+
+    /**
+     * Exécution de cette méthode avant chaque
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        $this->validator = new EmailValidator;
+    }
+
+    public function getProviders()
+    {
+        return [
+            ["moi@moi.com", true],
+            ["lui @lui.com", false],
+            ["emailsansarobase.com", false],
+            ["1111@222.33", false]
+        ];
+    }
+
+    /**
+     * @dataProvider getProviders
+     * 
+     *
+     * @param [type] $input
+     * @param [type] $expected
+     * @return void
+     */
+    public function testWithProviders($input, $expected)
+    {
+        $value = $this->validator->validate($input);
+        $this->assertEquals($expected, $value);
+    }
 
     public function testIfNullValueReturnsAnInvalidArgumentException()
     {
         $this->expectException(InvalidArgumentException::class);
-        $validator = new EmailValidator;
-        $val = $validator->validate(null);
+        $val = $this->validator->validate(null);
+    }
+
+    public function testIfEmptyValueReturnsAnInvalidArgumentException()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $val = $this->validator->validate("");
     }
 }
