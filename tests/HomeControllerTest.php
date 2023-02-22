12cc7d900edc918a49dfc9d8b5421a563eb39196
seb <contact@smaloron.eu>
2023-02-22 15:18:38 +0100

    test de testDAO

--- tests/HomeControllerTest.php
+++ tests/HomeControllerTest.php
@@ -24,4 +24,25 @@ class HomeControllerTest extends TestCase
             $response->getBody()
         );
     }
+
+    public function testOfTestDAO()
+    {
+        $dao = $this->createStub(SaleDAO::class);
+        $container = $this->createStub(ContainerInterface::class);
+        $dao->method("findAll")->willReturn($dao);
+        $dao->method("getAllAsArray")->willReturn([
+            ["id" => 1, "montant" => 300],
+            ["id" => 3, "montant" => 300],
+            ["id" => 5, "montant" => 300],
+        ]);
+        $container->method("get")->with("dao.sale")->willReturn($dao);
+
+        $controller = new HomeController($container);
+
+        $response = $controller->testDAO(new Response());
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertStringContainsString('"id":1', $response->getBody());
+        $this->assertCount(3, json_decode($response->getBody()));
+    }
 }
