e6a7090c414878482c570b7f80a5a2247b48b94f
seb <contact@smaloron.eu>
2023-02-23 15:29:05 +0100

    pattern observer avec eventName

--- public/design-pattern/observer.php
+++ public/design-pattern/observer.php
@@ -2,7 +2,7 @@
 
 interface SubscriberInterface
 {
-    public function update(string $message);
+    public function update(string $eventName, string $message);
 
     //public function getName(): string;
 }
@@ -11,42 +11,44 @@ abstract class AbstractPublisher
 {
     protected array $subscriberList = [];
 
-    public function addSubscriber(SubscriberInterface $sub)
+    public function addSubscriber(string $eventName, SubscriberInterface $sub)
     {
-        $this->subscriberList[] = $sub;
+        $this->subscriberList[$eventName][] = $sub;
     }
 
-    abstract public function notify(string $message): void;
+    abstract public function notify(string $eventName, string $message): void;
 }
 
 class Publisher extends AbstractPublisher
 {
-    public function notify(string $message): void
+    public function notify(string $eventName, string $message): void
     {
-        foreach ($this->subscriberList as $sub) {
-            $sub->update($message);
+        foreach ($this->subscriberList[$eventName] as $sub) {
+            $sub->update($eventName, $message);
         }
     }
 }
 
 class Logger implements SubscriberInterface
 {
-    public function update(string $message)
+    public function update(string $eventName, string $message)
     {
-        echo "\nJ'ai reçu notification de '$message' et je rentre cette info dans les logs\n";
+        echo "\n$eventName: J'ai reçu notification de '$message' et je rentre cette info dans les logs\n";
     }
 }
 
 class Emailer implements SubscriberInterface
 {
-    public function update(string $message)
+    public function update(string $eventName, string $message)
     {
-        echo "\nje vais envoyer '$message' à tous les membres de la liste de diffusion \n";
+        echo "\n$eventName: je vais envoyer '$message' à tous les membres de la liste de diffusion \n";
     }
 }
 
 $publisher = new Publisher();
-$publisher->addSubscriber(new Emailer);
-$publisher->addSubscriber(new Logger);
+$publisher->addSubscriber("mode.maintenance", new Emailer);
+$publisher->addSubscriber("mode.maintenance", new Logger);
+$publisher->addSubscriber("system.error", new Logger);
 
-$publisher->notify("Mode maintenance dans 5 minutes");
+$publisher->notify("mode.maintenance", "Mode maintenance dans 5 minutes");
+$publisher->notify("system.error", "On a encore oublié un point virgule");
