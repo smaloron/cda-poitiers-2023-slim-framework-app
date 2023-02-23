41247c9b329fd5d56263ff62806800866c935ca2
seb <contact@smaloron.eu>
2023-02-23 14:01:19 +0100

    pattern Decorator

---
+++ public/design-pattern/decorator.php
@@ -0,0 +1,77 @@
+<?php
+
+interface TileInterface
+{
+    public function getGold(): int;
+    public function getFood(): int;
+}
+
+class Tile implements TileInterface
+{
+
+    private string $type;
+
+
+    public function __construct(string $type)
+    {
+        $this->type = $type;
+    }
+
+    public function getGold(): int
+    {
+        return 0;
+    }
+
+    public function getFood(): int
+    {
+        return 0;
+    }
+}
+
+class TileDecorator implements TileInterface
+{
+
+
+
+    public function __construct(protected TileInterface $tile)
+    {
+    }
+
+    public function getGold(): int
+    {
+        return $this->tile->getGold();
+    }
+
+    public function getFood(): int
+    {
+        return $this->tile->getFood();
+    }
+}
+
+class WheatFieldDecorator extends TileDecorator
+{
+    public function getFood(): int
+    {
+        return 3 + $this->tile->getFood();
+    }
+}
+
+class CowHerdDecorator extends TileDecorator
+{
+    public function getFood(): int
+    {
+        return 4 + $this->tile->getFood();
+    }
+}
+
+
+
+$plain = new CowHerdDecorator(
+    new WheatFieldDecorator(
+        new Tile("plain")
+    )
+);
+echo $plain->getGold();
+echo $plain->getFood();
+
+$mountain = new Tile("mountain");
